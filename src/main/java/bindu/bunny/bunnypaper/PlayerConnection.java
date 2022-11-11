52f37fd7d501ba14831487c3de08432947fa56a7
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2022-11-10 21:11:48 -0500

    World Border initialization and gm not working yet but no errors

--- src/main/java/bindu/bunny/bunnypaper/PlayerConnection.java
+++ src/main/java/bindu/bunny/bunnypaper/PlayerConnection.java
@@ -4,32 +4,29 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.EnumWrappers;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.spigotmc.event.player.PlayerSpawnLocationEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.lang.reflect.InvocationTargetException;
 
 
 public class PlayerConnection implements Listener {
     @EventHandler
-    public void onPlayerJoin(PlayerSpawnLocationEvent event) throws InvocationTargetException {
+    public void onPlayerJoin(PlayerJoinEvent event) throws InvocationTargetException {
         Player player = event.getPlayer();
         ProtocolManager man = ProtocolLibrary.getProtocolManager();
 
         PacketContainer packet = man.createPacket(PacketType.Play.Server.INITIALIZE_BORDER);
 
         packet.getDoubles().
-                write(0, 0d).
-                write(1, 0d).
-                write(2, 1000d).
-                write(3, 20d);
+                write(0, player.getLocation().getX()).
+                write(1, player.getLocation().getZ()).
+                write(3, 10d);
         man.sendServerPacket(player, packet);
-
-        packet = new PacketContainer(PacketType.Play.Server.LOGIN);
-        packet.getGameModes().write(0, EnumWrappers.NativeGameMode.CREATIVE);
-        man.sendServerPacket(player, packet);
+//
+//        packet = new PacketContainer(PacketType.Play.Server.LOGIN);
+//        packet.getGameModes().write(0, EnumWrappers.NativeGameMode.CREATIVE);
     }
 }
