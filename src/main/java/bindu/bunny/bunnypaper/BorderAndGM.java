5a3e6bec79c66dba16fbeb22d0a7e96eacc266a1
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2022-11-11 14:39:06 -0500

    Not tested, changed from onPlayerConnection to PlayerChangedWorldEvent

---
+++ src/main/java/bindu/bunny/bunnypaper/BorderAndGM.java
@@ -0,0 +1,33 @@
+package bindu.bunny.bunnypaper;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+
+import java.lang.reflect.InvocationTargetException;
+
+
+public class BorderAndGM implements Listener {
+    @EventHandler
+    public void onChangeWorld(final PlayerChangedWorldEvent event) throws InvocationTargetException {
+        Player player = event.getPlayer();
+        ProtocolManager man = ProtocolLibrary.getProtocolManager();
+
+        PacketContainer packet = man.createPacket(PacketType.Play.Server.INITIALIZE_BORDER);
+
+        packet.getDoubles().
+                write(0, player.getLocation().getX()).
+                write(1, player.getLocation().getZ()).
+                write(3, 10d);
+        man.sendServerPacket(player, packet);
+
+       packet = new PacketContainer(PacketType.Play.Server.GAME_STATE_CHANGE);
+       packet.getGameModes().write(0, EnumWrappers.NativeGameMode.CREATIVE);
+       man.sendServerPacket(player, packet);
+    }
+}
